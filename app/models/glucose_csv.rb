4ff34ba276e731fafb5cc8be692763872a32ef76
Jeff Waltzer <jeffwaltzer@gmail.com>
2025-02-18 21:06:36 -0500

    parse first line of csv

--- app/models/glucose_csv.rb
+++ app/models/glucose_csv.rb
@@ -1,2 +1,18 @@
+require 'csv'
+
+
 class GlucoseCsv < ApplicationRecord
+  def glucose_measurements
+    rows =  CSV
+      .parse(csv
+              .split("\n")
+              .drop(1)
+              .join("\n"), headers: true)
+
+    first = rows.first.to_h
+    [
+      DateTime.strptime(first['Device Timestamp'] ,'%m-%d-%Y %k:%M %p'),
+      first['Historic Glucose mg/dL'].to_i
+    ]
+  end
 end
