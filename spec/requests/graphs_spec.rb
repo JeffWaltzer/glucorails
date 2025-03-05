fe52c3f5c5326b82bb1fd45b380c331de73a21da
John Maxwell <jmax@jmaxhome.com>
2025-03-04 21:51:17 -0500

    Started graph controller

--- spec/requests/graphs_spec.rb
+++ spec/requests/graphs_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Graphs", type: :request do
   describe "GET /show" do
+    fixtures :glucose_csvs
+
     it "renders SVG" do
-      get "/graph"
+      get "/graphs/1"
       expect(response.body).to include("<svg")
+    end
+
+    it 'does something. what?' do
+      get "/graphs/1"
+
+      expect(response.body).to (include("<line"))
     end
   end
 end
