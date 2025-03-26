38836ceb997bbb7352dda30539db858b6510ef4c
Jeff Waltzer <jeffwaltzer@gmail.com>
2025-03-25 21:51:18 -0400

    spec tidying

--- spec/requests/graphs_spec.rb
+++ spec/requests/graphs_spec.rb
@@ -31,28 +31,32 @@ RSpec.describe "Graphs", type: :request do
         xml
       end
 
+      let(:title) { xml.at_xpath('/svg/g/title') }
+      let(:svg) { xml.at_xpath('/svg').attributes }
+      let(:path) { xml.at_xpath('/svg/g/path').attributes }
+
       it 'has a title' do
-        expect(xml.at_xpath('/svg/g/title').text).to eq "Layer 1"
+        expect(title.text).to eq "Layer 1"
       end
 
       it 'has an svg width' do
-        expect(xml.at_xpath('/svg').attributes['width'].value).to eq "640"
+        expect(svg['width'].value).to eq "640"
       end
 
       it 'has an svg height' do
-        expect(xml.at_xpath('/svg').attributes['height'].value).to eq "480"
+        expect(svg['height'].value).to eq "480"
       end
 
       it 'has a path d' do
-        expect(xml.at_xpath('/svg/g/path').attributes['d'].value).to eq "m127,301l58,-69l121,-25l96,-39l68,229"
+        expect(path['d'].value).to eq "m127,301l58,-69l121,-25l96,-39l68,229"
       end
 
       it 'has a path stroke' do
-        expect(xml.at_xpath('/svg/g/path').attributes['stroke'].value).to eq '#000000'
+        expect(path['stroke'].value).to eq '#000000'
       end
 
       it 'has a path stroke_width' do
-        expect(xml.at_xpath('/svg/g/path').attributes['stroke-width'].value).to eq "5"
+        expect(path['stroke-width'].value).to eq "5"
       end
 
     end
