a5c0e81205e3fcfa491b32fce96a0659c7f4c06b
Jeff Waltzer <jeffwaltzer@gmail.com>
2025-04-08 22:08:41 -0400

    Extracted SVG Builder and started test driving it

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,28 +1,6 @@
 class GraphsController < ApplicationController
   def show
-    vg = Victor::SVG.new viewBox: '0 0 100 100',
-                        width: 640,
-                        height: 480
-    vg.g do
-      vg.title "Layer 1"
-      vg.path d: "m127,301l58,-69l121,-25l96,-39l68,229",
-              stroke: "#000000",
-              stroke_width: 5
-    end
-
-    render xml: vg.render
+    render xml: SvgBuilder.new.render_from_csv
   end
+
 end
-
-
-#     <<~SVG
-#     <?xml version="1.0"?>
-#     <svg width="640" height="480" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">
-#      <!-- Created with SVG-edit - https://github.com/SVG-Edit/svgedit-->
-#      <g class="layer">
-#       <title>Layer 1</title>
-#       <path d="m127,301l58,-69l121,-25l96,-39l68,229" fill="none" fill-opacity="null" id="svg_3" stroke="#000000" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-opacity="null" stroke-width="5"/>
-#      </g>
-#     </svg>
-# SVG
-
--- spec/lib/svg_builder_spec.rb
+++ spec/lib/svg_builder_spec.rb
@@ -1,7 +1,42 @@
-RSpec.describe SvgBuilder do
-  subject(:svg_builder) { described_class.new }
+# frozen_string_literal: true
+require 'rails_helper'
 
-  it "exists" do
-    expect(svg_builder).not_to be_nil
+RSpec.describe SvgBuilder do
+  subject(:svg_builder) {
+    described_class.new(
+    [
+      [DateTime.parse("02-14-2025 03:56 PM"), 308],
+      [DateTime.parse("02-14-2025 04:01 PM"), 308],
+      [DateTime.parse("02-14-2025 04:06 PM"), 299],
+      [DateTime.parse("02-14-2025 04:11 PM"), 295],
+      [DateTime.parse("02-14-2025 04:16 PM"), 308],
+      [DateTime.parse("02-14-2025 04:21 PM"), 302],
+      [DateTime.parse("02-14-2025 04:26 PM"), 300],
+    ]
+  )
+  }
+
+  let(:xml) do
+    xml = Nokogiri::XML.parse(svg_builder.render_from_csv) do |config|
+      config.options = Nokogiri::XML::ParseOptions::STRICT |
+        Nokogiri::XML::ParseOptions::DTDLOAD |
+        Nokogiri::XML::ParseOptions::DTDVALID
+    end
+    xml.remove_namespaces!
+    xml
   end
+
+  let(:svg) { xml.at_xpath('/svg').attributes }
+
+  it 'has an svg width' do
+    expect(svg['width'].value).to eq "640"
+  end
+
+  it 'has an svg height'
+
+  it 'has correct viewBox coordinates scaled to data'
+
+  it 'has a polyLine with correct coordinates'
+
+
 end
