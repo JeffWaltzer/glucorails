60c41e864ef95a5efb5c411857a62e63bd8c2081
John Maxwell <jmax@jmaxhome.com>
2025-04-15 21:23:50 -0400

    **BROKEN**
    Started drawing from real data in the controller

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,6 +1,12 @@
 class GraphsController < ApplicationController
   def show
-    # TODO: fix to use csv instead of 'junk'
-    render xml: SvgBuilder.new([]).render_from_csv
+    fake_data = [
+      [ DateTime.parse("2025-02-14T03:56+07:00"),  98 ],
+      [ DateTime.parse("2025-02-14T04:01+07:00"), 190 ],
+      [ DateTime.parse("2025-02-14T04:06+07:00"), 150 ],
+      [ DateTime.parse("2025-02-14T04:07+07:00"), 200 ]
+    ]
+
+    render xml: SvgBuilder.new(fake_data).render_from_csv
   end
 end
