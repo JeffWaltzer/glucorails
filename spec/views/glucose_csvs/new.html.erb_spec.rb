7ce9227ca664775ae49d01d7f3782496709046f8
John Maxwell <jmax@jmaxhome.com>
2025-05-20 16:25:00 -0400

    Removed dead specs for GlucoseCsvs

--- spec/views/glucose_csvs/new.html.erb_spec.rb
+++ spec/views/glucose_csvs/new.html.erb_spec.rb
@@ -2,15 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "glucose_csvs/new", type: :view do
   before(:each) do
-    assign(:glucose_csv, GlucoseCsv.new(
-      csv: "MyText"
-    ))
+    assign(:glucose_csv, "junk")
   end
 
   it "renders new glucose_csv form" do
     render
 
-    assert_select "form[action=?][method=?]", glucose_csvs_path, "post" do
+    assert_select "form[action=?][method=?]", new_glucose_csv_path, "post" do
       assert_select "input[type=file]"
     end
   end
