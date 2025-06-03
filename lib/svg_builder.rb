2422ff6f3c05a2117cc46594bd33391306fecd87
Jeff Waltzer <jeffwaltzer@gmail.com>
2025-06-03 16:05:16 -0400

    fix specs

--- lib/svg_builder.rb
+++ lib/svg_builder.rb
@@ -13,7 +13,7 @@ class SvgBuilder
   end
 
   def render_from_csv
-    svg_canvas = Victor::SVG.new viewbox: [ 0, 0, 1000, 500 ],
+    svg_canvas = Victor::SVG.new viewBox: [ 0, 0, 1000, 1000 ],
                                 preserveAspectRatio: :none,
                                 style: {width: "100%", height: "100%"}
     if @data.empty?
@@ -40,7 +40,7 @@ class SvgBuilder
     svg_canvas.rect x: 0,
                     y: 0,
                     width: 1000,
-                    height: 400,
+                    height: 1000,
                     fill: "PaleGreen"
 
     viewbox = [
