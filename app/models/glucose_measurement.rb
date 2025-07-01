bba17c86a3ddfabcb48e809935fcacf22a2c9020
Jeff Waltzer <jeffwaltzer@gmail.com>
2025-07-01 19:13:05 -0400

    add end_time to points_for

--- app/models/glucose_measurement.rb
+++ app/models/glucose_measurement.rb
@@ -1,11 +1,6 @@
 class GlucoseMeasurement < ApplicationRecord
   def self.points_for(start_time: nil, end_time: nil)
-    measurements = start_time.nil? ?
-      GlucoseMeasurement.all :
-      GlucoseMeasurement.where(measured_at: start_time..)
-
-    measurements.to_a.map do |measurement|
-      [measurement.measured_at, measurement.glucose]
-    end
+    measurements = GlucoseMeasurement.where(measured_at: start_time..end_time&.end_of_day)
+    measurements.pluck(:measured_at, :glucose)
   end
 end
