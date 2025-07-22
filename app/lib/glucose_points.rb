b7bad6b6523194a1c56e9e9dd431f8d9f434d86b
John Maxwell <jmax@jmaxhome.com>
2025-07-22 16:49:19 -0400

    Added time_integers

--- app/lib/glucose_points.rb
+++ app/lib/glucose_points.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
+
 class GlucosePoints
   attr_reader :points
 
   include Enumerable
 
+  def initialize(points)
+    @points = points
+  end
+
   def each
     return points.each unless block_given?
     points.each { |point| yield point }
     self
-  end
-
-  def initialize(points)
-    @points = points
   end
 
   def == (other)
@@ -34,12 +35,16 @@ class GlucosePoints
     @start_time ||= time_values.min
   end
 
+  def time_integers
+    time_values.map(&:to_i)
+  end
+
   def x_min
-    @x_min ||= time_values.map(&:to_i).min - start_time.to_i
+    @x_min ||= time_integers.min - start_time.to_i
   end
 
   def x_max
-    @x_max ||= time_values.map(&:to_i).max - start_time.to_i
+    @x_max ||= time_integers.max - start_time.to_i
   end
 
   def y_min
