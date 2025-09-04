67de89ea54587308051db7755aa695432119c5a0
Jeff Waltzer <jeffwaltzer@gmail.com>
2025-09-04 19:25:23 -0400

    Get rid of magic numbers

--- app/lib/svg_components/x_tic_mark.rb
+++ app/lib/svg_components/x_tic_mark.rb
@@ -1,23 +1,30 @@
 class SvgComponents::XTicMark < SvgComponents::TicMark
+  OFFSET_FOR_X_LABEL = 17
+  OFFSET_FOR_DATE_LABEL_Y = 25
+  OFFSET_FOR_TIME_LABEL_Y = 45
+
+  TICK_START = 9
+  TICK_END = 1
+
   def draw
     @svg_canvas.line class: "x-tick",
                      x1: SvgComponents::Base::scale_to_x(@index),
                      x2: SvgComponents::Base::scale_to_x(@index),
-                     y1: 959,
-                     y2: 949,
+                     y1: SvgComponents::PLOT_HEIGHT + TICK_START,
+                     y2: SvgComponents::PLOT_HEIGHT - TICK_END,
                      stroke: TIC_COLOR
 
     @svg_canvas.text x_tick_date_label,
-                     x: SvgComponents::Base::scale_to_x(@index) - 17,
-                     y: 975,
+                     x: SvgComponents::Base::scale_to_x(@index) - OFFSET_FOR_X_LABEL,
+                     y: SvgComponents::PLOT_HEIGHT + OFFSET_FOR_DATE_LABEL_Y,
                      style: "fill: #{TEXT_COLOR}",
                      class: "x-tick-date-label"
 
     @svg_canvas.text x_tick_time_label,
-                    x: SvgComponents::Base::scale_to_x(@index)-17,
-                    y: SvgComponents::PLOT_HEIGHT + 45,
-                    style: "fill: #{TEXT_COLOR}",
-                    class: "x-tick-time-label"
+                     x: SvgComponents::Base::scale_to_x(@index) - OFFSET_FOR_X_LABEL,
+                     y: SvgComponents::PLOT_HEIGHT + OFFSET_FOR_TIME_LABEL_Y,
+                     style: "fill: #{TEXT_COLOR}",
+                     class: "x-tick-time-label"
   end
 
   private
--- app/lib/svg_components/y_tic_mark.rb
+++ app/lib/svg_components/y_tic_mark.rb
@@ -1,8 +1,14 @@
 class SvgComponents::YTicMark < SvgComponents::TicMark
+
+  OFFSET_FOR_TEXT_X = 12
+  OFFSET_FOR_TEXT_Y = 5
+
+  TICK_WIDTH = 10
+
   def draw
     index = @number_of_tics - @index
     @svg_canvas.line class: "y-tick",
-                     x1: SvgComponents::PLOT_LEFT-10,
+                     x1: SvgComponents::PLOT_LEFT - TICK_WIDTH,
                      x2: SvgComponents::PLOT_LEFT,
                      y1: scale_to_y(index),
                      y2: scale_to_y(index),
@@ -12,14 +18,15 @@ class SvgComponents::YTicMark < SvgComponents::TicMark
 
   private
 
+
   def y_tick_label
     y_size = @svg_points.graph_y_max - @svg_points.graph_y_min
     y_position = y_size * @index / @number_of_tics.to_f
     glucose_value = (y_position + @svg_points.graph_y_min).round
 
     @svg_canvas.text glucose_value,
-                     x: 12,
-                     y: scale_to_y(@number_of_tics - @index) + 5 ,
+                     x: OFFSET_FOR_TEXT_X,
+                     y: scale_to_y(@number_of_tics - @index) + OFFSET_FOR_TEXT_Y,
                      style: "fill: #{TEXT_COLOR}",
                      class: "y-tick-label"
   end
